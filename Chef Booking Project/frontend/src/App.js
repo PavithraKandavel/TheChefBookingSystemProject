fd7286e9e6b89073fbe9cd1fabd0c828f56f8912
PavithraKandavel <pkandavel@my.okcu.edu>
2025-02-21 17:29:41 -0600

    initial commit

---
+++ Chef Booking Project/frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import ResetPassword from "./Pages/ResetPassword";
+import Home from "./Pages/Home";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/resetpassword" element={<ResetPassword/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
