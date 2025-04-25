884bd015dde5311cdca0b4ff262c01b63d047cfc
Pavithra Kandavel <pkandavel@my.okcu.edu>
2025-04-24 22:40:48 -0500

    added added complete admin controller with user, chef, booking, review, and notification management

--- frontend/admin/src/Layout/AdminLayout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import LayoutTemplate from "./LayoutPages/LayoutTemplate";
-
-
-const AdminLayout = ({ children }) => {
-  return <LayoutTemplate>{children}</LayoutTemplate>;
-};
-
-export default AdminLayout;
